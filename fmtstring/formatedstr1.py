3e3024168d8435c1de89351cb8f47c75d251b73a
Thanh Nguyen <chrisng9@outlook.com>
2022-04-25 15:21:55 -0700

    adding some type convertion example

---
+++ fmtstring/formatedstr1.py
@@ -0,0 +1,9 @@
+# Formated string 
+
+name = "Thanh"
+age = 50
+job = "programmer"
+
+print(f"{name} is {age}, he is a {job}")
+print("{} is {}, he is a {}".format("Thanh", 55, "Programmer"))
+print("{} is {}, he is a {}".format(name, age, job))
---
+++ stringindex/main.py
@@ -0,0 +1,14 @@
+# String index
+str_test = "Hello World"
+
+print(str_test[0:])
+print(str_test[2])
+print(str_test[2:4])
+# print(str_test[::-1])
+
+print(str_test.lower())
+# print(str_test.capitalize)
+
+print(str_test.upper())
+
+print(str_test.capitalize())
