0eb36f3b08ebeb88a7bfd9f415ed2d95440cf809
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-08-11 13:43:30 -0700

    relocate project directories and files

--- basics/fmtstring/formatedstr1.py
+++ basics/fmtstring/formatedstr1.py
@@ -5,5 +5,5 @@ age = 50
 job = "programmer"
 
 print(f"{name} is {age}, he is a {job}")
-print("{} is {}, he is a {}".format("Thanh", 55, "Programmer"))
+print("{} is {}, he is a {}".format("Thanh", 50, "Programmer"))
 print("{} is {}, he is a {}".format(name, age, job))
---
+++ basics/greeting/main.py
@@ -0,0 +1,10 @@
+print("Hello There!")
+
+name = input("What is your name? ")
+
+print("Hello ", name)
+
+place = input("where are you from? ")
+
+print("Nice")
+
