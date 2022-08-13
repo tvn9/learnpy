b8ec9e6da822af3719cdcb206662983a2c556990
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-08-12 22:00:23 -0700

    Adding dict examples

---
+++ basics/dict/dict1.py
@@ -0,0 +1,32 @@
+# Dictionary data type
+truck = {
+    "door": 4,
+    "wheel": 4,
+    "maxTrail": 9000,
+    "maxLoad": 2000,
+    "maxSpeed": "90mph",
+}
+
+print(truck["door"])
+print(truck)
+
+#
+dict = [
+    {
+        "key": "value",
+        "key2": 12345,
+    },
+    {
+        "key": "value1",
+        "key2": "value2",
+    }
+]
+
+#
+dict1 = {
+    "nums": [1,2,3,4,5,6],
+    "text": "more text here",
+}
+
+print(dict)
+print(dict1)
---
+++ basics/list/listunpacking.py
@@ -0,0 +1,6 @@
+# List unpacking
+
+a,b,c, *other, onemore = [0,1,2,3,4,5,6,7,8,9]
+
+print(a, b, c, other)
+print(onemore)
